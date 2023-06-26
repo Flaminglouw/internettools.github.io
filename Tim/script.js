e06713b8e7f8b07eadc1476a6fbfd540e1f8dff6
Flaminglouw <132774069+Flaminglouw@users.noreply.github.com>
2023-06-26 13:04:57 +0200

    Big Final 1

--- Tim/script.js
+++ Tim/script.js
@@ -1,3 +1,4 @@
+//Slideshow
 let slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
 
@@ -15,4 +16,19 @@ function nextSlide() {
 setInterval(nextSlide, 5000); // Wechselt alle 5 Sekunden zum nächsten Bild
 
 showSlide(0); // Zeige das erste Bild an
+
+//Button
+window.addEventListener('scroll', function() {
+  var scrollTopButton = document.querySelector('.scroll-top-button');
+  if (window.pageYOffset > 300) {
+    scrollTopButton.classList.add('active');
+  } else {
+    scrollTopButton.classList.remove('active');
+  }
+});
+
+document.querySelector('.scroll-top-button').addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
---
+++ tim/Über uns/script.js
@@ -0,0 +1,33 @@
+//Slideshow
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides[currentSlide].style.display = 'none';
+  slides[index].style.display = 'block';
+  currentSlide = index;
+}
+
+function nextSlide() {
+  let newIndex = (currentSlide + 1) % slides.length;
+  showSlide(newIndex);
+}
+
+setInterval(nextSlide, 5000); // Wechselt alle 5 Sekunden zum nächsten Bild
+
+showSlide(0); // Zeige das erste Bild an
+
+//Button
+window.addEventListener('scroll', function() {
+  var scrollTopButton = document.querySelector('.scroll-top-button');
+  if (window.pageYOffset > 300) {
+    scrollTopButton.classList.add('active');
+  } else {
+    scrollTopButton.classList.remove('active');
+  }
+});
+
+document.querySelector('.scroll-top-button').addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
