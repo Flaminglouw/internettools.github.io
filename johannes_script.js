6afce7db6662bcd48be95e2bf4c9a3a65d1e4a8a
Flaminglouw <132774069+Flaminglouw@users.noreply.github.com>
2023-07-02 14:32:54 +0200

    Update johannes_script.js

--- johannes_script.js
+++ johannes_script.js
@@ -63,22 +63,26 @@ setTimeout(function () {
 }, 6000); // Start the slideshow after 6 seconds
 
 // Menu of the restaurant Modul script//
-const openModalButtons = document.querySelectorAll(".modalbutton");
-const closeModalButtons = document.querySelectorAll("[data-close-button]");
-const overlay = document.getElementById("overlay");
+const openModalButtons = document.querySelectorAll(".modalbutton"); // Konstante ruft Elementen mit der Klasse modalbutton auf.
+const closeModalButtons = document.querySelectorAll("[data-close-button]"); // Konstante ruft Attribut data-close button aus dem DOM auf. Object Model (Dokumentenobjektmodell)
+const overlay = document.getElementById("overlay"); // Konstante Element ID overlay
 
 openModalButtons.forEach((button) => {
+  // forEach-Schleife alle Elmente in openModalbuttons zu iterieren.
   button.addEventListener("click", () => {
-    const modalId = button.getAttribute("data-modal-target");
-    const modal = document.querySelector(modalId);
-    openModal(modal);
+    // Klick event reagieren
+    const modalId = button.getAttribute("data-modal-target"); // wert durch klicken von data-modal-target abgerufen.
+    const modal = document.querySelector(modalId); // document.querySelector Modal Element anhand seine ID in Konstante modal gespeichert.
+    openModal(modal); // Funktion openModal open Modal-Element und CSS Klasse
   });
 });
 
 overlay.addEventListener("click", () => {
-  const modals = document.querySelectorAll(".modal.active");
+  // EventListener für Klick-Event- sollen alle aktiven Modal-Elemente geschlossen werden.
+  const modals = document.querySelectorAll(".modal.active"); // alle Elmenten Klasse modal und active in Konsante modals gespeichert-Aktive FENSTER
   modals.forEach((modal) => {
-    closeModal(modal);
+    // forEach -Loop Schleife alle AKTIVEN Elementen zu iterieren.
+    closeModal(modal); // Modal ausgeblendet CSS Klasse wird entfernt
   });
 });
 
@@ -91,47 +95,51 @@ closeModalButtons.forEach((button) => {
 
 function openModal(modal) {
   if (modal == null) return;
-  modal.classList.add("active");
+  modal.classList.add("active"); // füght die Klasse active zu and aktivieren den Modal
   overlay.classList.add("active");
 }
 
 function closeModal(modal) {
   if (modal == null) return;
-  modal.classList.remove("active");
+  modal.classList.remove("active"); // Klasse active wird entfernen
   overlay.classList.remove("active");
 }
 
 // date for placeholder
 
 document.addEventListener("DOMContentLoaded", function () {
+  // EventListerer das DOM Ereignis hört. HTML vollständig geladen ist und bereit ist zu manipulieren.
   // Code to set the default date value
-  const dateInput = document.getElementById("dateInput");
-  const currentDate = new Date();
-  const formattedDate = currentDate.toISOString().slice(0, 16);
-  dateInput.value = formattedDate;
+  const dateInput = document.getElementById("dateInput"); // Eingabefeld mit ID "dateInput" wird abgerufen und in Konstante gespeicher
+  const currentDate = new Date(); // Neues Date-Objekt mit aktuellem Datum wird erstellt.
+  const formattedDate = currentDate.toISOString().slice(0, 16); // Aktuelles Datum wird in formatierten String umgewandelt. Slice let me extract a portion of the sting
+  dateInput.value = formattedDate; // Formatierter Datumswert wird dem Eingabefeld zugewiesen.
 });
 
 // Function to handle the intersection changes
 function handleIntersection(entries, observer) {
+  //  Funktion, die aufgerufen wird, wenn der Zustand der Intersection-Veränderungen erfasst wird.
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("animate-fly-in");
-      observer.unobserve(entry.target);
+      // Überprüft, ob das Element sichtbar (intersecting) ist.
+      entry.target.classList.add("animate-fly-in"); //  Fügt dem Element eine CSS-Klasse hinzu, um eine animierte Einflugbewegung zu starten.
+      observer.unobserve(entry.target); //  Stoppt die Beobachtung des Elements durch den Intersection Observer.
 
-      const overlayImage = document.querySelector(".overlay-image2");
-      overlayImage.style.opacity = 1;
-      overlayImage.style.animation = "delayedAppear 2.5s forwards";
+      const overlayImage = document.querySelector(".overlay-image2"); // Ruft das Element mit der Klasse ".overlay-image2" ab.
+      overlayImage.style.opacity = 1; //  Setzt die Opazität des Elements auf 1, um es sichtbar zu machen.
+      overlayImage.style.animation = "delayedAppear 2.5s forwards"; // Weist dem Element eine CSS-Animation zu, die nach einer Verzögerung von 2,5 Sekunden startet.
     }
   });
 }
 
 // Create an intersection observer instance
 const options = {
+  //  Optionen für den Intersection Observer, z. B. die Schwelle und die rootMargin.
   threshold: 1, // Adjust the threshold as needed
   rootMargin: "0px 0px 0px 0px", // Add negative bottom margin to delay triggering
 };
 
-const observer = new IntersectionObserver(handleIntersection, options);
+const observer = new IntersectionObserver(handleIntersection, options); // Erstellt eine Instanz des Intersection Observers mit der angegebenen Callback-Funktion und Optionen.
 
 // Get the .fly element
 const flyElement = document.querySelector(".fly");
